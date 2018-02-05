f151a7f8f0d6a9f96822f59d40160b6aa8e5cd0c
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-02-05 13:25:21 +0530

    Create mohan4.c

---
+++ mohan4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+ 
+void main()
+{
+   int i = 5;
+ 
+   if ( (1 <= i <= 5) )
+      printf( "1 to 5\n" );}
+      {
+ 
+   if ( 6 <= i <= 7 )
+      printf( "6 to 7\n" );
+ 
+   if ( 8 <= i <= 10 )
+      printf( "8 to 10\n" );
+ }
+ {
+   getch();
+   return 0;
+}
