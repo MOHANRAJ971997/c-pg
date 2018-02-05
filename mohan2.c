f0141e246be760c24a0c948db24442cc4ac93f2a
MOHANRAJ971997 <35914794+MOHANRAJ971997@users.noreply.github.com>
2018-02-05 13:16:49 +0530

    Create mohan2.c

---
+++ mohan2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+ 
+int main()
+{
+   char a[100], b[100];
+ 
+   printf("Enter a string\n");
+   gets(a);
+ 
+   printf("Enter a string\n");
+   gets(b);
+ 
+   if (strcmp(a,b) == 0)
+      printf("Entered strings are equal.\n");
+   else
+      printf("Entered strings are not equal.\n");
+ 
+   return 0;
+}
+{
+
+getch();
+}
